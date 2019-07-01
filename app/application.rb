c4d34060f794b953686b0a36fedb59ae7bd60afb
Chinedu Arinze <c.s.arinze@gmail.com>
2019-07-01 15:04:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,5 +3,13 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      resp.write "Do something"
+    else
+      resp.write "Path Not Found"
+      resp.status = 404
+    end
+    resp.finsh
   end
 end
